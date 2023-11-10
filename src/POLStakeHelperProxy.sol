cbb7a3fe2f1bdb0a1bdd2f7de886cd31fec65a70
omnifient <90161786+omnifient@users.noreply.github.com>
2023-11-10 02:10:33 +0000

    add contract for proxy (wip)
    using a transparent proxy as per requirements

---
+++ src/POLStakeHelperProxy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract POLStakeHelperProxy is TransparentUpgradeableProxy {
+    constructor(
+        address impl_,
+        bytes memory data_
+    ) payable TransparentUpgradeableProxy(impl_, msg.sender, data_) {
+        // TODO: TBD (initialOWner)
+    }
+}
