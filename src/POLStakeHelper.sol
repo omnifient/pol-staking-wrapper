1fe854ab5a86cf513775aec00d62b9e9c0c9ed5e
omnifient <90161786+omnifient@users.noreply.github.com>
2023-11-13 17:06:57 +0000

    setup access control with oz

--- src/POLStakeHelper.sol
+++ src/POLStakeHelper.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 import "./interfaces/IPolygonMigration.sol";
 import "./interfaces/IStakeManager.sol";
 
 /// @title POLStakeHelper
 /// @notice TBW
-contract POLStakeHelper {
+contract POLStakeHelper is AccessControl {
     using SafeERC20 for IERC20;
+
+    bytes32 public constant ROLE_ADMIN = keccak256("ADMIN");
+    bytes32 public constant ROLE_OPERATOR = keccak256("OPERATOR");
 
     /// @notice POL token
     IERC20 immutable pol;
@@ -30,9 +34,17 @@ contract POLStakeHelper {
     /// @notice beneficiary for rewards and unstake
     address public beneficiary;
 
-    // TODO: admin, operator roles
+    modifier onlyAdminOrOperator() {
+        require(
+            hasRole(ROLE_ADMIN, msg.sender) ||
+                hasRole(ROLE_OPERATOR, msg.sender),
+            "NOT_ALLOWED"
+        );
+        _;
+    }
 
     constructor(
+        address admin,
         address pol_,
         address matic_,
         address polMigrator_,
@@ -48,12 +60,13 @@ contract POLStakeHelper {
         delegate = delegate_;
         beneficiary = beneficiary_;
 
-        // TODO: set admin
+        // configure access control
+        _setRoleAdmin(ROLE_OPERATOR, ROLE_ADMIN); // set ROLE_ADMIN as the admin role for ROLE_OPERATOR
+        _grantRole(ROLE_ADMIN, admin); // grant ROLE_ADMIN to `admin`
     }
 
     /// @notice TBW
-    function stakePOL(uint256 amount) external {
-        // TODO: onlyAdminOrOperator
+    function stakePOL(uint256 amount) external onlyAdminOrOperator {
         require(amount > 0, "INVALID_AMT");
 
         // transfer POL from sender to this contract
@@ -66,16 +79,17 @@ contract POLStakeHelper {
         matic.approve(address(stakingManager), amount);
 
         // TODO: stake the MATIC
-        // stakingManager.stake(amount, ..) // how to use the delegate?
+        // stakingManager.stake(amount, ..) // TODO: how to use the delegate?
     }
 
     /// @notice TBW
-    function unstakePOL(uint256 amount) external {
-        // TODO: onlyAdminOrOperator
+    function unstakePOL(uint256 amount) external onlyAdminOrOperator {
         require(amount > 0, "INVALID_AMT");
 
-        // TODO: unstake the MATIC
-        // stakingManager.unstake(amount, ..) where is delegate used?
+        // get the validator id (TODO: maybe store this in the contract after staking?)
+        uint256 validatorId = stakingManager.getValidatorId(address(this));
+        // unstake the MATIC
+        stakingManager.unstake(validatorId); // TODO: where is delegate used?
 
         // allow the migrator to spend the MATIC - TODO: do this once with max amount?
         matic.approve(address(polMigrator), amount);
@@ -88,9 +102,7 @@ contract POLStakeHelper {
     }
 
     /// @notice TBW
-    function claimRewards() external {
-        // TODO: onlyAdminOrOperator
-
+    function claimRewards() external onlyAdminOrOperator {
         // get the validator id (TODO: maybe store this in the contract after staking?)
         uint256 validatorId = stakingManager.getValidatorId(address(this));
         // withdraw the rewards from staking
@@ -109,29 +121,11 @@ contract POLStakeHelper {
     }
 
     /// @notice TBW
-    function setBeneficiary(address newBeneficiary) external {
-        // TODO: onlyAdmin
-
+    function setBeneficiary(
+        address newBeneficiary
+    ) external onlyRole(ROLE_ADMIN) {
         require(newBeneficiary != address(0), "INVALID_ADDR");
 
         beneficiary = newBeneficiary;
-    }
-
-    /// @notice TBW
-    function addOperator(address newOperator) external {
-        // TODO: use access control library?
-        // TODO: TBI
-        // TODO: onlyAdmin
-
-        require(newOperator != address(0), "INVALID_ADDR");
-    }
-
-    /// @notice TBW
-    function removeOperator(address op) external {
-        // TODO: use access control library?
-        // TODO: TBI
-        // TODO: onlyAdmin
-
-        require(op != address(0), "INVALID_ADDR");
     }
 }
