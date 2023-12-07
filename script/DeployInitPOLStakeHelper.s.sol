93ef8ae09e6bad47714ee4d73fd5d1a1476de1e7
omnifient <90161786+omnifient@users.noreply.github.com>
2023-12-07 11:24:28 +0000

    minor refactor to initialization process, separating the delegate setting to its own function
    this is because creating a delegate requires the POLStakeHelper's address

--- script/DeployInitPOLStakeHelper.s.sol
+++ script/DeployInitPOLStakeHelper.s.sol
@@ -4,8 +4,16 @@ pragma solidity ^0.8.23;
 import "lib/forge-std/src/Script.sol";
 
 import "./DeployLib.sol";
+import "../src/POLStakeHelperProxy.sol";
+import "../src/POLStakeHelper.sol";
 
-contract DeployInitPOLStakeHelper is Script {
+/// @notice Deploy and initialization scripts for POLStakeHelper
+/// Initialization is a two step process because of the delegate:
+/// creating a delegate requires the address of the POLStakeHelper,
+/// but POLStakeHelper needs the delegate.
+/// As such, the delegate is set after the deploy+init of POLStakeHelper.
+
+contract DeployInit1POLStakeHelper is Script {
     function run() external {
         vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
 
@@ -14,19 +22,39 @@ contract DeployInitPOLStakeHelper is Script {
         address pol = vm.envAddress("TOKEN_POL");
         address matic = vm.envAddress("TOKEN_MATIC");
         address polygonMigrator = vm.envAddress("POLYGON_MIGRATOR");
-        address delegate = vm.envAddress("DELEGATE");
         address beneficiary = vm.envAddress("BENEFICIARY");
         address stakeManager = vm.envAddress("STAKE_MANAGER");
 
-        DeployLib.deploy(
+        // deploy and initialize the impl+proxy
+        address polStakeHelperProxyAddr = DeployLib.deploy(
             admin,
             pol,
             matic,
             polygonMigrator,
-            delegate,
             beneficiary,
             stakeManager
         );
+
+        vm.setEnv(
+            "POL_STAKE_HELPER_PROXY",
+            vm.toString(polStakeHelperProxyAddr)
+        );
+
+        vm.stopBroadcast();
+    }
+}
+
+contract Init2POLStakeHelper is Script {
+    function run() external {
+        vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+
+        // NOTE: this assumes that the deployer is an admin or operator
+
+        // post-set the delegate field
+        address polStakeHelperProxy = vm.envAddress("POL_STAKE_HELPER_PROXY");
+        address delegate = vm.envAddress("DELEGATE");
+
+        POLStakeHelper(polStakeHelperProxy).setDelegate(delegate);
 
         vm.stopBroadcast();
     }
