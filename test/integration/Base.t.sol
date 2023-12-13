7f99357f79e61102e068e194c290ec45dbca73b0
nogo <0xnogo@proton.me>
2023-12-13 14:32:13 +0400

    Base test setup

--- test/integration/Base.t.sol
+++ test/integration/Base.t.sol
@@ -33,7 +33,6 @@ contract Base is Test {
         IStakeManager stakeManager = IStakeManager(stakeManagerAddr);
 
         vm.startPrank(deployer);
-
         // deploy and (partly) initialize the POLStakeHelper
         address polStakeHelperProxyAddr = DeployLib.deploy(
             _admin,
@@ -44,11 +43,25 @@ contract Base is Test {
             stakeManagerAddr
         );
         _polStakeHelper = POLStakeHelper(polStakeHelperProxyAddr);
+        vm.stopPrank();
 
-        // -------------
-        bytes memory signerPubKey; // TODO: validator key e.g. vm.envBytes("TEST_SIGNER_PUB_KEY")
-        // pre-requisite: create a validatorshare for our POLStakeHelper
-        // TODO: change some state to be able to call stakeFor
+        bytes memory signerPubKey = vm.envBytes("TEST_SIGNER_PUB_KEY");
+
+        address validatorAddress = _getSigner(signerPubKey);
+
+        vm.prank(0x2f7E0aeCE1df77277504d885246cd98704372758); // at 18762829: 3 MATIC
+        _matic.transfer(validatorAddress, 3 * 10 ** 18);
+
+        assertEq(_matic.balanceOf(validatorAddress), 3 * 10 ** 18);
+
+        // prank the stake manager address and increase the validator threshold
+        address governanceAddress = 0x6e7a5820baD6cebA8Ef5ea69c0C92EbbDAc9CE48;
+        uint256 currentThreshold = stakeManager.validatorThreshold();
+        vm.prank(governanceAddress);
+        stakeManager.updateValidatorThreshold(currentThreshold + 1);
+
+        vm.startPrank(validatorAddress);
+        _matic.approve(stakeManagerAddr, 3 * 10 ** 18);
         stakeManager.stakeFor(
             polStakeHelperProxyAddr,
             10 ** 18, // min amount
@@ -59,10 +72,14 @@ contract Base is Test {
         address delegate = stakeManager.getValidatorContract(
             stakeManager.getValidatorId(polStakeHelperProxyAddr)
         );
-        // -------------
-
-        _polStakeHelper.setDelegate(delegate);
-
         vm.stopPrank();
+
+        vm.prank(_admin);
+        _polStakeHelper.setDelegate(delegate);
+    }
+
+    function _getSigner(bytes memory pub) private pure returns (address) {
+        require(pub.length == 64, "not pub");
+        return address(uint160(uint256(keccak256(pub))));
     }
 }
